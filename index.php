46ba1c814260b7a1096791788c92caf30de255f7
Yellow <yellowaformac@gmail.com>
2018-01-26 11:13:06 +0100

    initial push

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <script type="text/javascript" src="Librairie/jquery.js"></script>
+    <script type="text/javascript" src ="script.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <link href="Librairie/bootstrap.min.css">
+    <meta charset="utf-8">
+    <title>Index Commun</title>
+  </head>
+  <body>
+
+
+  <nav>
+    <button type="button" name="Retour" class="Retour">
+    </button>
+    <div class="Chemin d'où on est">
+    </div>
+  </nav>
+
+  <div class="Arbo">
+  </div>
+
+  </body>
+</html>
