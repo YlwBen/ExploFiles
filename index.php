0fcb5fe9bcff9204b331375b4e52bef105684f4e
Ascherion <johan.desplats@gmail.com>
2018-01-26 14:03:51 +0100

    commit3joh

--- index.php
+++ index.php
@@ -1,28 +1,23 @@
 <!DOCTYPE html>
 <html>
-  <head>
-  	<script type="text/javascript" src="Librairie/jquery.js"></script>
-  	<script type="text/javascript" src ="script.js"></script>
-  	<link rel="stylesheet" href="Librairie/bootstrap/css/bootstrap.css">
-  	<link rel="stylesheet" href="style.css">
-  	<meta charset="utf-8">
-    <title>Index Commun</title>
-    
-   
-  </head>
-  <body>
 
- 
-  <nav class="navbar navbar-dark bg-primary">
-    <form class="form-inline col-12">
-      <input class="form-control col-10 mr-sm-3" type="url">
-      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Retour</button>
-    </form>
-  </nav>
+ <head>
+   <script type="text/javascript" src="Librairie/jquery.js"></script>
+   <script type="text/javascript" src="script.js"></script>
+   <script rel="stylesheet" href="bouton.css"></script>
+   <link rel="stylesheet" href="Librairie/bootstrap.min.css">
+   <meta charset="utf-8">
+   <title>Index Commun</title>
+ </head>
 
+ <body>
+   <nav class="navbar navbar-dark bg-primary">
+     <form class="form-inline col-12">
+       <input class="form-control col-8" type="url">
+       <a href="#" class="btn btn-primary btn-primary col-4">
+       <span class="glyphicon glyphicon-arrow-left"></span> Retour</a>
+     </form>
+   </nav>
+ </body>
 
-  <div class="Arbo">
-  </div>
-
-  </body>
 </html>
