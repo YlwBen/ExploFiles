f03b67332f199355462f5e90fc85c6173aef2e5b
Ascherion <johan.desplats@gmail.com>
2018-01-26 12:19:08 +0100

    commit1joh

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <script type="text/javascript" src="Librairie/jquery.js"></script>
-    <script type="text/javascript" src ="script.js"></script>
-    <link rel="stylesheet" href="style.css">
-    <link href="Librairie/bootstrap.min.css">
-    <meta charset="utf-8">
+  	<script type="text/javascript" src="Librairie/jquery.js"></script>
+  	<script type="text/javascript" src ="script.js"></script>
+  	<link rel="stylesheet" href="Librairie/bootstrap/css/bootstrap.css">
+  	<link rel="stylesheet" href="style.css">
+  	<meta charset="utf-8">
     <title>Index Commun</title>
+    
+   
   </head>
   <body>
 
-
-  <nav>
-    <button type="button" name="Retour" class="Retour">
-    </button>
-    <div class="Chemin d'où on est">
-    </div>
+ 
+  <nav class="navbar navbar-dark bg-primary">
+    <form class="form-inline">
+      <input class="form-control mr-sm-3" type="url" value="http://">
+      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Retour</button>
+    </form>
   </nav>
+
 
   <div class="Arbo">
   </div>
